e69d52f9451055e60da06723ca404cecafaf6c9a
Zach Thomae <zthomae@gmail.com>
2016-01-31 16:34:12 -0600

    Add current ip alias

--- shell/bash_aliases.sh
+++ shell/bash_aliases.sh
@@ -1,2 +1,5 @@
 # Open any Xcode project that is in the current working directory
 alias openx='find . -d 1 | grep xcodeproj | head -n 1 | xargs open --fresh --background'
+
+# Get current ip address
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
